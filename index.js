178e50822d8ad91e219b9912b80497ebf53a0e2b
Rukonju <rukonbds@gmail.com>
2021-10-29 00:28:00 +0600

    server-test

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Courier server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`Running URL: http//:localhost:${port}`)
+})
